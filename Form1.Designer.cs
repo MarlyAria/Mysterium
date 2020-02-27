766c450f6c75d2d1ffd4ce6fed10ce175ee82eaa
MarlyAria <60437606+MarlyAria@users.noreply.github.com>
2020-02-27 09:46:55 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace Mysterium
+{
+    partial class MysteriumChoose
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SpelersCB = new System.Windows.Forms.ComboBox();
+            this.labelPhychics = new System.Windows.Forms.Label();
+            this.labelGhostState = new System.Windows.Forms.Label();
+            this.GhostStateCB = new System.Windows.Forms.ComboBox();
+            this.checkBoxSecretLies = new System.Windows.Forms.CheckBox();
+            this.checkBoxHiddenSigns = new System.Windows.Forms.CheckBox();
+            this.Promo1CB = new System.Windows.Forms.CheckBox();
+            this.Promo2CB = new System.Windows.Forms.CheckBox();
+            this.Promo3CB = new System.Windows.Forms.CheckBox();
+            this.Promo4CB = new System.Windows.Forms.CheckBox();
+            this.Promo5CB = new System.Windows.Forms.CheckBox();
+            this.Promo6CB = new System.Windows.Forms.CheckBox();
+            this.labelObjectStory = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.LabelExpensions = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SpelersCB
+            // 
+            this.SpelersCB.AllowDrop = true;
+            this.SpelersCB.FormattingEnabled = true;
+            this.SpelersCB.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6"});
+            this.SpelersCB.Location = new System.Drawing.Point(735, 387);
+            this.SpelersCB.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.SpelersCB.Name = "SpelersCB";
+            this.SpelersCB.Size = new System.Drawing.Size(99, 38);
+            this.SpelersCB.TabIndex = 0;
+            // 
+            // labelPhychics
+            // 
+            this.labelPhychics.AutoSize = true;
+            this.labelPhychics.BackColor = System.Drawing.Color.Transparent;
+            this.labelPhychics.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labelPhychics.Location = new System.Drawing.Point(165, 393);
+            this.labelPhychics.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.labelPhychics.Name = "labelPhychics";
+            this.labelPhychics.Size = new System.Drawing.Size(554, 32);
+            this.labelPhychics.TabIndex = 1;
+            this.labelPhychics.Text = "How many phychics are going to help the Ghost";
+            this.labelPhychics.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // labelGhostState
+            // 
+            this.labelGhostState.AutoSize = true;
+            this.labelGhostState.BackColor = System.Drawing.Color.Transparent;
+            this.labelGhostState.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labelGhostState.Location = new System.Drawing.Point(271, 455);
+            this.labelGhostState.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.labelGhostState.Name = "labelGhostState";
+            this.labelGhostState.Size = new System.Drawing.Size(448, 32);
+            this.labelGhostState.TabIndex = 2;
+            this.labelGhostState.Text = "Choose the mentale state of the Ghost";
+            this.labelGhostState.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // GhostStateCB
+            // 
+            this.GhostStateCB.FormattingEnabled = true;
+            this.GhostStateCB.Items.AddRange(new object[] {
+            "A bit confused, but still know what he\'s doing(Easy)",
+            "Fairly confused, things are a bit hazy(Normal)",
+            "Compleetly bonkers (Hard)"});
+            this.GhostStateCB.Location = new System.Drawing.Point(738, 449);
+            this.GhostStateCB.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.GhostStateCB.Name = "GhostStateCB";
+            this.GhostStateCB.Size = new System.Drawing.Size(481, 38);
+            this.GhostStateCB.TabIndex = 3;
+            // 
+            // checkBoxSecretLies
+            // 
+            this.checkBoxSecretLies.AutoSize = true;
+            this.checkBoxSecretLies.BackColor = System.Drawing.Color.Transparent;
+            this.checkBoxSecretLies.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.checkBoxSecretLies.Location = new System.Drawing.Point(927, 605);
+            this.checkBoxSecretLies.Name = "checkBoxSecretLies";
+            this.checkBoxSecretLies.Size = new System.Drawing.Size(217, 36);
+            this.checkBoxSecretLies.TabIndex = 4;
+            this.checkBoxSecretLies.Text = "Secrets and Lies";
+            this.checkBoxSecretLies.UseVisualStyleBackColor = false;
+            // 
+            // checkBoxHiddenSigns
+            // 
+            this.checkBoxHiddenSigns.AutoSize = true;
+            this.checkBoxHiddenSigns.BackColor = System.Drawing.Color.Transparent;
+            this.checkBoxHiddenSigns.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.checkBoxHiddenSigns.Location = new System.Drawing.Point(738, 605);
+            this.checkBoxHiddenSigns.Name = "checkBoxHiddenSigns";
+            this.checkBoxHiddenSigns.Size = new System.Drawing.Size(184, 36);
+            this.checkBoxHiddenSigns.TabIndex = 5;
+            this.checkBoxHiddenSigns.Text = "Hidden Signs";
+            this.checkBoxHiddenSigns.UseVisualStyleBackColor = false;
+            // 
+            // Promo1CB
+            // 
+            this.Promo1CB.AutoSize = true;
+            this.Promo1CB.BackColor = System.Drawing.Color.Transparent;
+            this.Promo1CB.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Promo1CB.Location = new System.Drawing.Point(738, 659);
+            this.Promo1CB.Name = "Promo1CB";
+            this.Promo1CB.Size = new System.Drawing.Size(127, 36);
+            this.Promo1CB.TabIndex = 6;
+            this.Promo1CB.Text = "Promo 1";
+            this.Promo1CB.UseVisualStyleBackColor = false;
+            // 
+            // Promo2CB
+            // 
+            this.Promo2CB.AutoSize = true;
+            this.Promo2CB.BackColor = System.Drawing.Color.Transparent;
+            this.Promo2CB.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Promo2CB.Location = new System.Drawing.Point(881, 659);
+            this.Promo2CB.Name = "Promo2CB";
+            this.Promo2CB.Size = new System.Drawing.Size(129, 36);
+            this.Promo2CB.TabIndex = 7;
+            this.Promo2CB.Text = "Promo 2";
+            this.Promo2CB.UseVisualStyleBackColor = false;
+            // 
+            // Promo3CB
+            // 
+            this.Promo3CB.AutoSize = true;
+            this.Promo3CB.BackColor = System.Drawing.Color.Transparent;
+            this.Promo3CB.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Promo3CB.Location = new System.Drawing.Point(1029, 659);
+            this.Promo3CB.Name = "Promo3CB";
+            this.Promo3CB.Size = new System.Drawing.Size(130, 36);
+            this.Promo3CB.TabIndex = 8;
+            this.Promo3CB.Text = "Promo 3";
+            this.Promo3CB.UseVisualStyleBackColor = false;
+            // 
+            // Promo4CB
+            // 
+            this.Promo4CB.AutoSize = true;
+            this.Promo4CB.BackColor = System.Drawing.Color.Transparent;
+            this.Promo4CB.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Promo4CB.Location = new System.Drawing.Point(735, 701);
+            this.Promo4CB.Name = "Promo4CB";
+            this.Promo4CB.Size = new System.Drawing.Size(130, 36);
+            this.Promo4CB.TabIndex = 9;
+            this.Promo4CB.Text = "Promo 4";
+            this.Promo4CB.UseVisualStyleBackColor = false;
+            // 
+            // Promo5CB
+            // 
+            this.Promo5CB.AutoSize = true;
+            this.Promo5CB.BackColor = System.Drawing.Color.Transparent;
+            this.Promo5CB.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Promo5CB.Location = new System.Drawing.Point(881, 701);
+            this.Promo5CB.Name = "Promo5CB";
+            this.Promo5CB.Size = new System.Drawing.Size(130, 36);
+            this.Promo5CB.TabIndex = 10;
+            this.Promo5CB.Text = "Promo 5";
+            this.Promo5CB.UseVisualStyleBackColor = false;
+            // 
+            // Promo6CB
+            // 
+            this.Promo6CB.AutoSize = true;
+            this.Promo6CB.BackColor = System.Drawing.Color.Transparent;
+            this.Promo6CB.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Promo6CB.Location = new System.Drawing.Point(1029, 701);
+            this.Promo6CB.Name = "Promo6CB";
+            this.Promo6CB.Size = new System.Drawing.Size(131, 36);
+            this.Promo6CB.TabIndex = 11;
+            this.Promo6CB.Text = "Promo 6";
+            this.Promo6CB.UseVisualStyleBackColor = false;
+            // 
+            // labelObjectStory
+            // 
+            this.labelObjectStory.AutoSize = true;
+            this.labelObjectStory.BackColor = System.Drawing.Color.Transparent;
+            this.labelObjectStory.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labelObjectStory.Location = new System.Drawing.Point(246, 511);
+            this.labelObjectStory.Name = "labelObjectStory";
+            this.labelObjectStory.Size = new System.Drawing.Size(473, 64);
+            this.labelObjectStory.TabIndex = 12;
+            this.labelObjectStory.Text = "Are you searching for the murder object\r\nor the story behind the murder?\r\n";
+            this.labelObjectStory.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "The murder object",
+            "The story of how the ghost got killed"});
+            this.comboBox1.Location = new System.Drawing.Point(738, 520);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(445, 38);
+            this.comboBox1.TabIndex = 13;
+            // 
+            // LabelExpensions
+            // 
+            this.LabelExpensions.AutoSize = true;
+            this.LabelExpensions.BackColor = System.Drawing.Color.Transparent;
+            this.LabelExpensions.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.LabelExpensions.Location = new System.Drawing.Point(115, 605);
+            this.LabelExpensions.Name = "LabelExpensions";
+            this.LabelExpensions.Size = new System.Drawing.Size(604, 32);
+            this.LabelExpensions.TabIndex = 14;
+            this.LabelExpensions.Text = "Choose with Expansions you are adding to the game";
+            this.LabelExpensions.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // MysteriumChoose
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.BackgroundImage = global::Mysterium.Properties.Resources.Mysterium_banner;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1419, 845);
+            this.Controls.Add(this.LabelExpensions);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.labelObjectStory);
+            this.Controls.Add(this.Promo6CB);
+            this.Controls.Add(this.Promo5CB);
+            this.Controls.Add(this.Promo4CB);
+            this.Controls.Add(this.Promo3CB);
+            this.Controls.Add(this.Promo2CB);
+            this.Controls.Add(this.Promo1CB);
+            this.Controls.Add(this.checkBoxHiddenSigns);
+            this.Controls.Add(this.checkBoxSecretLies);
+            this.Controls.Add(this.GhostStateCB);
+            this.Controls.Add(this.labelGhostState);
+            this.Controls.Add(this.labelPhychics);
+            this.Controls.Add(this.SpelersCB);
+            this.Font = new System.Drawing.Font("Miriam Libre", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "MysteriumChoose";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox SpelersCB;
+        private System.Windows.Forms.Label labelPhychics;
+        private System.Windows.Forms.Label labelGhostState;
+        private System.Windows.Forms.ComboBox GhostStateCB;
+        private System.Windows.Forms.CheckBox checkBoxSecretLies;
+        private System.Windows.Forms.CheckBox checkBoxHiddenSigns;
+        private System.Windows.Forms.CheckBox Promo1CB;
+        private System.Windows.Forms.CheckBox Promo2CB;
+        private System.Windows.Forms.CheckBox Promo3CB;
+        private System.Windows.Forms.CheckBox Promo4CB;
+        private System.Windows.Forms.CheckBox Promo5CB;
+        private System.Windows.Forms.CheckBox Promo6CB;
+        private System.Windows.Forms.Label labelObjectStory;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label LabelExpensions;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mysterium
+{
+    public partial class MysteriumChoose : Form
+    {
+        public MysteriumChoose()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        /* 2 = 4,5,6
+         * 3 = 5,6,7
+         * 4 = 5,6,7
+         * 5 = 5,7,8
+         * 6 = 6,8,9
+         * 7 = 8,9,10
+         * */
+
+
+
+    }
+}
